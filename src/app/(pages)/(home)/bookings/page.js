5d610b29d00d1c1f547a51eceab30abab58d9ad6
sujonphero <sujonsheikh.in@gmail.com>
2024-08-12 22:12:00 +0600

    rebuild7

--- src/app/(pages)/(home)/bookings/page.js
+++ src/app/(pages)/(home)/bookings/page.js
@@ -1,9 +1,12 @@
 import PastBooking from "@/components/user/booking/PastBooking";
 import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
 import ProfileInfo from "@/components/user/ProfileInfo";
-import { getBookingsByUser, getUserByEmail } from "@/database/queries";
 import { redirect } from "next/navigation";
 import { auth } from "../../../../../auth";
+import {
+  getBookingsByUser,
+  getUserByEmail,
+} from "../../../../database/queries/index";
 
 const BookingsPage = async () => {
   const session = await auth();
--- src/app/(pages)/(home)/hotels/[id]/payment/page.js
+++ src/app/(pages)/(home)/hotels/[id]/payment/page.js
@@ -1,8 +1,11 @@
 import PaymentForm from "@/components/payto/paymentForm";
-import { getHotelById, getUserByEmail } from "@/database/queries";
 import { getDayDifference } from "@/utils/data-utils";
 import { redirect } from "next/navigation";
 import { auth } from "../../../../../../../auth";
+import {
+  getHotelById,
+  getUserByEmail,
+} from "../../../../../../database/queries/index";
 
 const PaymentPage = async ({
   params: { id },
